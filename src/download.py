699595cbf6fe502e26e4aa3f869ce8d03a343ff0
Shannon Lal <shannonlal@hotmail.com>
2022-10-07 22:29:28 -0400

    Update download.py
    Adding print

--- src/download.py
+++ src/download.py
@@ -5,6 +5,7 @@ import clip
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     model, transform = clip.load("ViT-B/32", device="cpu")
+    print('Downloaded Model')
 
 if __name__ == "__main__":
     download_model()
